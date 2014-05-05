15bfe123d8e54aa0e14b30c724acf30477f41146
Alexander Persson <alexander.persson@jayway.com>
2014-05-05 15:22:27 +0200

    lab1 solution

--- UniversalComponent/UniversalComponent.Shared/ViewModels/ItemVM.cs
+++ UniversalComponent/UniversalComponent.Shared/ViewModels/ItemVM.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Windows.UI.Popups;
 
 namespace UniversalComponent.ViewModels
 {
-    public class ItemVm : ViewModelBase
+    public partial class ItemVM : ViewModelBase
     {
         private string _title;
         public string Title
@@ -55,7 +57,7 @@ namespace UniversalComponent.ViewModels
             get { return _buttonCommand ?? (_buttonCommand = new RelayCommand(ButtonAction, () => true)); }
         }
 
-        public ItemVm()
+        public ItemVM()
         {
             Title = "Cool sheep";
             Details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut malesuada lacus. Nulla at fringilla tortor, in dictum mi. Nulla fermentum lorem eu metus tempor, vel vulputate orci congue. Sed dapibus ante in diam feugiat, sed vulputate eros pulvinar. Aliquam ultrices fringilla fermentum. Cras consequat suscipit accumsan. Aliquam non leo vestibulum, tempus urna eu, aliquet quam. Cras sit amet rutrum augue, a rhoncus purus. Pellentesque tempor laoreet erat, eu pretium mauris facilisis eu. Suspendisse fermentum feugiat magna, id pulvinar justo sollicitudin id. Maecenas sit amet vulputate nibh. In hac habitasse platea dictumst. Nullam in pulvinar ligula. Donec pellentesque tempus felis.";
@@ -64,7 +66,8 @@ namespace UniversalComponent.ViewModels
 
         private async void ButtonAction()
         {
-            //DoTheStuff
+            var msg = new MessageDialog("DoTheStuff!");
+            await msg.ShowAsync();
         }
     }
 }
---
+++ UniversalComponent/UniversalComponent.Windows/ViewModels/ItemVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+using Windows.UI.Popups;
+
+namespace UniversalComponent.ViewModels
+{
+    public partial class ItemVM
+    {
+        private ICommand _anotherButtonCommand;
+        public ICommand AnotherButtonCommand
+        {
+            get { return _anotherButtonCommand ?? (_anotherButtonCommand = new RelayCommand(AnotherButtonAction, () => true)); }
+        }
+
+        private async void AnotherButtonAction()
+        {
+            var msg = new MessageDialog("DoMoreStuff!");
+            await msg.ShowAsync();
+        }
+    }
+}
